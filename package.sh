09a41b133dabbedc9a187b35b278503bc80f1721
killzoms <13115936+killzoms@users.noreply.github.com>
2026-05-05 14:02:25 -0400

    Bump Version

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+VERSION=$(cat $DIR/info.json | jq -r '.version')
+NAME=$(cat $DIR/info.json | jq -r '.name')
+
+mkdir -p $DIR/Build/$NAME\_$VERSION
+for file in $DIR/*; do
+    if [[ $file != *package.sh* && $file != *Build* ]]; then
+        cp -R $file $DIR/Build/$NAME\_$VERSION/
+    fi
+done
